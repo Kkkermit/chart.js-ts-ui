d6b52905043eb0a3bbd906c030891a58c235f5b4
Joshua Eaton <jeaton446@gmail.com>
2024-06-25 14:32:05 +0100

    add: Added SAVE_VOTES to redux store, updated unit testing

--- ts-ui/src/redux/action/action.redux.test.ts
+++ ts-ui/src/redux/action/action.redux.test.ts
@@ -1,7 +1,42 @@
+import { configureStore } from "@reduxjs/toolkit";
 import { RESET_STORE, resetStore } from "./action.redux";
-import { describe, it, expect } from "vitest";
+import voteReducer from "../slice/vote.slice";
+import { fetchVotes, saveVotes } from "./action.redux";
+import { describe, it, expect, beforeEach } from "vitest";
 
 describe("action.redux", () => {
+	let store: any;
+
+	beforeEach(() => {
+		store = configureStore({
+			reducer: {
+				votes: voteReducer,
+			},
+		});
+		localStorage.clear();
+	});
+
+	it("fetches votes from local storage", () => {
+		const data = {
+			chartType: "pie",
+			votes: [],
+		};
+		localStorage.setItem("data", JSON.stringify(data));
+		store.dispatch(fetchVotes());
+		expect(store.getState().votes).toEqual(data);
+	});
+
+	it("saves votes to local storage", () => {
+		const data = {
+			chartType: "pie",
+			votes: [],
+		};
+		store.dispatch(saveVotes(data.votes));
+		const storedData = JSON.parse(localStorage.getItem("data") || "[]");
+		expect(storedData).toEqual(data.votes);
+		expect(store.getState().votes.votes).toEqual(data.votes);
+	});
+
 	it("should create an action to reset the store", () => {
 		const expectedAction = {
 			type: RESET_STORE,
--- ts-ui/src/redux/store/vote.store.ts
+++ ts-ui/src/redux/store/vote.store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, Dispatch, Action } from "@reduxjs/toolkit";
 import votesReducer from "../slice/vote.slice";
 
 export const store = configureStore({
@@ -6,6 +6,18 @@ export const store = configureStore({
 		votes: votesReducer,
 	},
 });
+
+type Votes = {
+	chartType: string;
+	votes: number[];
+};
+
+export const saveVotes = (votes: Votes) => {
+	return (dispatch: Dispatch<Action<string>>) => {
+		dispatch({ type: "SAVE_VOTES", payload: votes });
+		localStorage.setItem("data", JSON.stringify(votes));
+	};
+};
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
